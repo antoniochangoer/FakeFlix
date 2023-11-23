792f8d29b740bc97bf24dbc1b3a7174fbc69f2a8
nishal89 <nishal_adhin@hotmail.com>
2023-11-23 20:50:41 +0100

    add font without exo

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -7,10 +7,9 @@
         <title inertia>{{ config('app.name', 'Laravel') }}</title>
 
         <!-- Fonts -->
-        <link rel="preconnect" href="https://fonts.googleapis.com">
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-        <link href="https://fonts.googleapis.com/css2?family=Exo:wght@400;500;700&family=Outfit:wght@300;400;500;700&display=swap" rel="stylesheet">
-
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Outfit:wght@300;400;500;700&display=swap" rel="stylesheet">
         <!-- Scripts -->
         @routes
         @viteReactRefresh
