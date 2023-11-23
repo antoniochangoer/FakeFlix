c8d1d97e64caf80fd4b1f5eff7f2b2b6ac58a924
antoniochangoer <54006126+antoniochangoer@users.noreply.github.com>
2023-11-23 20:48:51 +0100

    tailwind init

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,19 +1,25 @@
-import defaultTheme from 'tailwindcss/defaultTheme';
-import forms from '@tailwindcss/forms';
+import forms from "@tailwindcss/forms";
 
 /** @type {import('tailwindcss').Config} */
 export default {
     content: [
-        './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
-        './storage/framework/views/*.php',
-        './resources/views/**/*.blade.php',
-        './resources/js/**/*.jsx',
+        "./vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php",
+        "./storage/framework/views/*.php",
+        "./resources/views/**/*.blade.php",
+        "./resources/js/**/*.jsx",
     ],
 
     theme: {
         extend: {
-            fontFamily: {
-                sans: ['Figtree', ...defaultTheme.fontFamily.sans],
+            color: {
+                "brand-red": "#FC4747",
+                "brand-dark-blue": "#10141E",
+                "brand-grey-blue": "#5A698F",
+                "brand-semi-blue": "#161D2F",
+            },
+            fontSize: {
+                "body-m": "15px",
+                "body-s": "13px",
             },
         },
     },
