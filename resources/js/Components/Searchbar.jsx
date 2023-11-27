066c3a380f09c2697b969c82da0b22b4f6cd1778
antoniochangoer <54006126+antoniochangoer@users.noreply.github.com>
2023-11-28 00:53:36 +0100

    clean up

--- resources/js/Components/Searchbar.jsx
+++ resources/js/Components/Searchbar.jsx
@@ -3,27 +3,33 @@ import InputLabel from "@/Components/InputLabel";
 
 export default function Searchbar() {
     return (
-        <form className="mt-6 mx-4 md:mx-10 3xl:mt-14 3xl:mx-10 relative">
-            <div className="cursor-pointer absolute inset-y-0 left-0 flex items-center">
-                <MagnifyingGlassIcon
-                    className="h-5 fill-white"
-                    aria-hidden="true"
-                    strokeWidth={4}
+        <>
+            <form className="mt-6 mx-4 md:mx-10 3xl:mt-14 3xl:mx-10 relative">
+                <div className="cursor-pointer absolute inset-y-0 left-0 flex items-center">
+                    <MagnifyingGlassIcon
+                        className="h-5 fill-white"
+                        aria-hidden="true"
+                        strokeWidth={4}
+                    />
+                </div>
+                <InputLabel
+                    className="sr-only"
+                    htmlFor="search"
+                    value="Search"
                 />
-            </div>
-            <InputLabel className="sr-only" htmlFor="search" value="Search" />
-            <input
-                autoComplete="off"
-                type="text"
-                name="search"
-                id="search"
-                className={`peer font-light pl-8 bg-brand-dark-blue block w-full border-0 text-inherit shadow-sm ring-inset ring-brand-dark-blue placeholder:text-gray-400  focus:ring-2 focus:ring-inset focus:ring-brand-dark-blue text-base caret-brand-red md:text-xl`}
-                placeholder="Search for movies or TV series"
-            />
-            <div
-                className={`absolute inset-x-0 bottom-0 border-t border-transparent peer-focus:border-t-[1px] peer-focus:border-brand-grey-blue`}
-                aria-hidden="true"
-            ></div>
-        </form>
+                <input
+                    autoComplete="off"
+                    type="text"
+                    name="search"
+                    id="search"
+                    className={`peer font-light pl-8 bg-brand-dark-blue block w-full border-0 text-inherit shadow-sm ring-inset ring-brand-dark-blue placeholder:text-gray-400  focus:ring-2 focus:ring-inset focus:ring-brand-dark-blue text-base caret-brand-red md:text-xl`}
+                    placeholder="Search for movies or TV series"
+                />
+                <div
+                    className={`absolute inset-x-0 bottom-0 border-t border-transparent peer-focus:border-t-[1px] peer-focus:border-brand-grey-blue`}
+                    aria-hidden="true"
+                ></div>
+            </form>
+        </>
     );
 }
--- resources/js/Pages/Dashboard.jsx
+++ resources/js/Pages/Dashboard.jsx
@@ -10,31 +10,12 @@ export default function Dashboard({ auth, movies }) {
         <AuthenticatedLayout user={auth.user}>
             <Head title="Dashboard" />
 
-            {/* Searchbar think about where to handle the initial result */}
+            {/* TODO: Searchbar gets movies and uses that data to filter out based on the search result */}
+            {/* TODO: Searchresult maybe as a child of searchbar? easy to pass to children instead of shared parent */}
+            {/* TODO: Show either search result when typing OR the default rendered page with && or ? */}
             <Searchbar />
 
             {/* <h1>MAIN CONTENT</h1> */}
         </AuthenticatedLayout>
     );
 }
-
-// <div>
-//     {/* Iterate over the contents and display them */}
-//     {contents.map((content) => (
-//         <div key={content.id}>
-//             <h2 className="text-white">{content.title}</h2>
-//             {/* Display the thumbnails */}
-//             {/* Ensure the structure matches how you have stored it in the Content model */}
-//             {content.thumbnail && content.thumbnail.regular && (
-//                 <div>
-//                     <img
-//                         src={content.thumbnail.regular.small}
-//                         alt="Regular Small Thumbnail"
-//                     />
-//                     {/* Similarly for medium and large thumbnails */}
-//                 </div>
-//             )}
-//             {/* Add more content details here */}
-//         </div>
-//     ))}
-// </div>
