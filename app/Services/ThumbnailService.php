6c7019372bd46b5d93ef51e0002fe996354b106c
nishal89 <nishal_adhin@hotmail.com>
2023-11-28 00:09:24 +0100

    bug fix thumbnail services

--- app/Services/ThumbnailService.php
+++ app/Services/ThumbnailService.php
@@ -7,11 +7,17 @@ class ThumbnailService
     public static function transformThumbnails($movie)
     {
         if (isset($movie->thumbnail_trending) && is_string($movie->thumbnail_trending)) {
-            $movie->thumbnail_trending = self::transformThumbnailPaths(json_decode($movie->thumbnail_trending, true));
+            $decodedThumbnails = json_decode($movie->thumbnail_trending, true);
+            if (is_array($decodedThumbnails)) {
+                $movie->thumbnail_trending = self::transformThumbnailPaths($decodedThumbnails);
+            }
         }
 
         if (isset($movie->thumbnail_regular) && is_string($movie->thumbnail_regular)) {
-            $movie->thumbnail_regular = self::transformThumbnailPaths(json_decode($movie->thumbnail_regular, true));
+            $decodedThumbnails = json_decode($movie->thumbnail_regular, true);
+            if (is_array($decodedThumbnails)) {
+                $movie->thumbnail_regular = self::transformThumbnailPaths($decodedThumbnails);
+            }
         }
 
         return $movie;
@@ -19,9 +25,13 @@ class ThumbnailService
 
     private static function transformThumbnailPaths($thumbnails)
     {
-        foreach ($thumbnails as $type => $sizes) {
-            foreach ($sizes as $size => $path) {
-                $thumbnails[$type][$size] = asset($path);
+        if (is_array($thumbnails)) {
+            foreach ($thumbnails as $type => &$sizes) {
+                if (is_array($sizes)) {
+                    foreach ($sizes as $size => $path) {
+                        $sizes[$size] = asset($path);
+                    }
+                }
             }
         }
         return $thumbnails;
